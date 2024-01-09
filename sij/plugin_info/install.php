2112674e3ce3006221f4724dbd2f04d3aacc947e
bryan-pignerol <90550463+bryan-pignerol@users.noreply.github.com>
2024-01-09 12:00:05 +0100

    Modifications
    -MAJ du plugin_info/info.json
    -Fichiers renommés

--- sij/plugin_info/install.php
+++ sij/plugin_info/install.php
@@ -18,13 +18,13 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 // Fonction exécutée automatiquement après l'installation du plugin
-function template_install() {
+function sij_install() {
 }
 
 // Fonction exécutée automatiquement après la mise à jour du plugin
-function template_update() {
+function sij_update() {
 }
 
 // Fonction exécutée automatiquement après la suppression du plugin
-function template_remove() {
+function sij_remove() {
 }
